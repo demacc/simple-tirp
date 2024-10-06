dd41f2300624e882bf05be458fb2c537ce11aa0c
Thein Htike Aung <senku9123@gmail.com>
2024-10-06 22:39:37 +0630

    useEffectDepencyarrayUses

--- src/components/tripList/index.js
+++ src/components/tripList/index.js
@@ -2,18 +2,28 @@ import React, { useEffect, useState } from "react";
 
 export default function TirpList() {
   let [trips, setTrips] = useState([]);
+
+  let [url, setUrl] = useState("http://localhost:3001/tirp");
   //useEffect api fetch yin infinty loop ma pyit ag use ya
   useEffect(() => {
-    fetch("http://localhost:3001/tirp")
+    fetch(url)
       .then((res) => res.json())
       .then((data) => {
         setTrips(data);
       });
-  }, []);
+  }, [url]); //depency arr ka tt chr ka ny url ko update loke tae ah kr components mout pyn pyit aug loke py
 
   return (
     <div>
       <h1>Ready to go?</h1>
+      <button onClick={() => setUrl("http://localhost:3001/tirp")}>
+        All trips
+      </button>
+      <button
+        onClick={() => setUrl("http://localhost:3001/tirp?location=Myanmar")}
+      >
+        Tirp to Myanmar
+      </button>
       <ul>
         {trips.map((trip) => (
           <li key={trip.id}>
